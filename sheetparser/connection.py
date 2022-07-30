b51d6c3931b9b7390a09669410176aa1d5e11dae
alfisnig <valerasafin2005@gmail.com>
2022-07-30 21:55:52 +0300

    connection to sheet API added

---
+++ sheetparser/connection.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import gspread
+from config import CREDENTIALS_FILE_PATH, SPREADSHEET_ID, PARSING_RANGE
+
+
+def get_sheet() -> gspread.Spreadsheet:
+    service_account = gspread.service_account(CREDENTIALS_FILE_PATH)
+    sheet = service_account.open_by_key(SPREADSHEET_ID)
+    return sheet
+
+
+def get_work_sheet(title: str) -> gspread.Worksheet:
+    sheet = get_sheet()
+    work_sheet = sheet.worksheet(title)
+    return work_sheet
