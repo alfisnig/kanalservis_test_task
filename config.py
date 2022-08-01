68f087e27652a91357cc1caf352f4b8b3dd38787
alfisnig <valerasafin2005@gmail.com>
2022-08-01 20:42:13 +0300

    config example addded

---
+++ config.py
@@ -0,0 +1,28 @@
+import os.path
+
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+# Google API
+CREDENTIALS_FILE_NAME = ''
+CREDENTIALS_FILE_PATH = os.path.join(ROOT_DIR, CREDENTIALS_FILE_NAME)
+SPREADSHEET_ID = ''
+
+
+# PostgreSQL
+PG_USER = ''
+DB_NAME = ''
+PG_PASSWORD = ''
+PG_HOST = '127.0.0.1'
+PG_PORT = 5432
+
+# sheetparser
+PARSING_RANGE = 20
+SHEET_MONITORING_DELAY = 30
+
+# notifications
+DELIVERY_MONITORING_DELAY = 60
+
+# Telegram bot
+API_TOKEN = ''
